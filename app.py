39bbdea7bad57c5c7ce09021ce18ed483f652741
Thawkster <tristan@nithon.com>
2019-10-07 10:03:47 -0700

    [ADD] Users can see all stock

--- app.py
+++ app.py
@@ -7,5 +7,16 @@ def index():
     """Return homepage."""
     return render_template('home.html', msg='Flask is Cool!!')
 
+# OUR MOCK ARRAY OF PROJECTS
+stock = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
+@app.route('/stock')
+def stock_index():
+    """Show all stock."""
+    return render_template('stock_index.html', stock=stock)
+
 if __name__ == '__main__':
     app.run(debug=True)
