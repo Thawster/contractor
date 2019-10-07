b50cde3b6a0d9f230a1c1a8a018d1d5fbafe4e31
Thawkster <tristan@nithon.com>
2019-10-07 10:09:56 -0700

    [ADD] MongoDB and stock model

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Contractor
+stock = db.stock
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+#@app.route('/')
+#def index():
+#    """Return homepage."""
+#    return render_template('home.html', msg='Flask is Cool!!')
 
 # OUR MOCK ARRAY OF PROJECTS
 stock = [
@@ -13,10 +18,10 @@ stock = [
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ]
 
-@app.route('/stock')
+@app.route('/')
 def stock_index():
     """Show all stock."""
-    return render_template('stock_index.html', stock=stock)
+    return render_template('stock_index.html', stock=stock.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
