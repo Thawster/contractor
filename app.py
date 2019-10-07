7c56e6085a4117fee623242ee83ded2174c616bd
Thawkster <tristan@nithon.com>
2019-10-07 11:03:49 -0700

    [ADD] Users can create items

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 client = MongoClient()
@@ -13,15 +13,27 @@ app = Flask(__name__)
 #    return render_template('home.html', msg='Flask is Cool!!')
 
 # OUR MOCK ARRAY OF PROJECTS
-stock = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
 
 @app.route('/')
 def stock_index():
     """Show all stock."""
     return render_template('stock_index.html', stock=stock.find())
+
+@app.route('/stock', methods=['POST'])
+def stock_submit():
+    """Submit a new item."""
+    item = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description')
+        'costs' : request.form.get('costs').split()
+    }
+    stock.insert_one(item)
+    return redirect(url_for('stock_index'))
+
+@app.route('/stock/new')
+def stock_new():
+    """Create a new item."""
+    return render_template('stock_new.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
