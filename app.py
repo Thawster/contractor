c46d815dfe36d3342569de5992a58186a022bbba
Thawkster <tristan@nithon.com>
2019-10-09 15:24:02 -0700

    [ADD] port fallback

--- app.py
+++ app.py
@@ -71,3 +71,5 @@ def stock_delete(item_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #If port problem appears
+    #app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
